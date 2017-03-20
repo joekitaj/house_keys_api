3bcc941bf787bf695040c5486023fd36aba21aa8
Joe Kitaj <joekitaj@gmail.com>
2017-03-20 09:15:16 -0400

    Reposition connection in db

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 // Allows for connection to database
 const pgp = require('pg-promise')();
+const connection = process.env.DATABASE_URL || 'postgress://localhost:5432/forum';
 
 // Specifies database
 const db = pgp(connection);
 
-const connection = process.env.DATABASE_URL || 'postgress://localhost:5432/forum';
 
 module.exports = db;
