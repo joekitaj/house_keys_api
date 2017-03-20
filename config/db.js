027f11e0a69ec071b1fd022737daa7181b51b86d
Joe Kitaj <joekitaj@gmail.com>
2017-03-20 09:56:03 -0400

    Change DB

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 // Allows for connection to database
 const pgp = require('pg-promise')();
-const connection = process.env.DATABASE_URL || 'postgress://localhost:5432/forum';
+const connection = process.env.DATABASE_URL || 'postgress://localhost:5432/house_keys';
 
 // Specifies database
 const db = pgp(connection);
